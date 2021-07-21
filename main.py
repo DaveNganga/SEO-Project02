ab496896f31d2dd8a681238c12cb34c9bbdac397
Raekwon Adams-Rodgers <adamsrodgers@etsu.edu>
2021-07-21 20:58:11 +0000

    added faq file with potential of adding more

--- main.py
+++ main.py
@@ -44,7 +44,12 @@ def dashboard():
 
 @app.route("/help")    #Home page (I just put something here as a placeholder)
 def help():
-    return render_template('faq.html')
+    return render_template('faq.html', subtitle = 'FAQ', text1 = 'When will I get my remind Email?',
+                          text2 = 'You will get your remind email 24 hours befroe the next billing date.',
+                          text3 = 'How many app can I register on my account',
+                          text4 = 'You can add as many as you would like.',
+                          text5 = 'Can I cancel my account?',
+                          text6 = 'Yes, you can cancel your account at anytime.')
 
 @app.route("/home_logged")    #Home page (I just put something here as a placeholder)
 def loggedHome():
