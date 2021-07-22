32e1620ee56befe6ae98fa173734567f3fe9cb5d
Dave Ng'ang'a <dn789@cairn.edu>
2021-07-22 15:14:07 +0000

    main edits

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from form import InputForm
 from flask_sqlalchemy import SQLAlchemy
-
+from models import *
 app = Flask(__name__, template_folder='templates')
 
 app.config['SECRET_KEY'] = '6849ddabd6659cb932e048b24afa64d71de66d5a9b4a5974'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-class User(db.Model): #creation of database
-    id = db.Column(db.Integer, primary_key=True)
-    name_of_subscription = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    billing_cycle = db.Column(db.Integer, nullable=False)
-    cost = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.email}','{self.name_of_subscription}', '{self.billing_cycle}', {self.cost})" 
-
 
 @app.route("/")    #Home page (I just put something here as a placeholder)
 def home():
@@ -27,7 +16,7 @@ def home():
 def register():
     form = InputForm()
     if form.validate_on_submit(): #what happens when the user signs up for our reminder
-        user = User(name_of_subscription=form.name_of_subscription.data, email=form.email.data, billing_cycle=form.billing_cycle.data, cost = form.cost.data)
+        user = user(email=form.name.data, name=form.name.data, password=form.password.data,)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
---
+++ models.py
@@ -0,0 +1,20 @@
+from main import *
+
+class User(db.Model): #creation of database
+    id = db.Column(db.Integer, primary_key=True) 
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return f"User('{self.email}','{self.name_of_subscription}', '{self.billing_cycle}', {self.cost})" 
+
+class Subscription(db.Model): #creation of database
+    id = db.Column(db.Integer, primary_key=True)
+    name_of_subscription = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    billing_cycle = db.Column(db.Integer, nullable=False)
+    cost = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.email}','{self.name_of_subscription}', '{self.billing_cycle}')"
